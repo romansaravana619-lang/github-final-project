f80ca2a0bac801515dc3136a7ba57e8d52034841
Saravana Kumar M <romansaravana619@gmail.com>
2026-09-22 13:44:30 +0530

    Add assessment URL routes

---
+++ django-online-course-final/onlinecourse/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+app_name = "onlinecourse"
+
+urlpatterns = [
+    path("", views.CourseListView.as_view(), name="index"),
+    path("registration/", views.registration_request, name="registration"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
+    path("<int:pk>/", views.CourseDetailView.as_view(), name="course_details"),
+    path("<int:course_id>/enroll/", views.enroll, name="enroll"),
+    # Task 6: submit route
+    path("<int:course_id>/submit/", views.submit, name="submit"),
+    # Task 6: show_exam_result route
+    path(
+        "course/<int:course_id>/submission/<int:submission_id>/result/",
+        views.show_exam_result,
+        name="show_exam_result",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
